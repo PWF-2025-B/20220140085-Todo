84667cc0e2353d2f4a7e3aef3bd8bb3821ba2d9a
agilimam <agilimamalhafizi04@gmail.com>
2025-05-03 11:33:02 +0700

    middleware, gate admin

--- routes/web.php
+++ routes/web.php
@@ -5,34 +5,56 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Todo2Controller;
 use App\Http\Controllers\UserController;
 
-Route::resource('todo', Todo2Controller::class)->except(['show']);
-
+// Halaman awal
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Dashboard hanya untuk user yang login dan terverifikasi
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+// Grup route untuk user yang sudah login
 Route::middleware('auth')->group(function () {
+
+    //Profile User
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Route untuk Todo
+    Route::resource('todo', Todo2Controller::class)->except(['show']);
+    Route::delete('/todo', [Todo2Controller::class, 'destroyCompleted'])->name('todo.destroyCompleted');
+    Route::patch('/todo/{todo}/complete', [Todo2Controller::class, 'complete'])->name('todo.complete');
+    Route::patch('/todo/{todo}/uncomplete', [Todo2Controller::class, 'uncomplete'])->name('todo.uncomplete');
 });
 
 
+// Grup route untuk admin
+Route::middleware(['auth', 'admin'])->group(function () {
+
+    Route::resource('/user',UserController::class)->except('show');
+    Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
+    Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
+
+});
+
+// Route untuk menampilkan semua todo
 Route::patch('/todo/{todo}/complete', [Todo2Controller::class, 'complete'])->name('todo.complete');
 Route::patch('/todo/{todo}/uncomplete', [Todo2Controller::class, 'uncomplete'])->name('todo.uncomplete');
 
-
+// Route untuk menampilkan semua user
 Route::get('/user',[UserController::class, 'index'])->name('user.index');
 Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
 Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
 
-Route::get('/todo/{todo}/edit', [Todo2Controller::class, 'edit'])->name('todo.edit');
-Route::patch('/todo/{todo}', [Todo2Controller::class, 'update'])->name('todo.update');
 
+// Route untuk Edit todo
+Route::get('/todo/{todo}/edit', [Todo2Controller::class, 'edit'])->name('todo.edit');
+
+// Route untuk menghapus todo
 Route::delete('/todo/{todo}', [Todo2Controller::class, 'destroy'])->name('todo.destroy');
 Route::delete('/todo', [Todo2Controller::class, 'destroyCompleted'])->name('todo.destroyCompleted');
 Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
---
+++ tugas7.md
@@ -0,0 +1,5 @@
+## Middleware & Gate Admin Not Active
+![alt text](<screenshot/tugas7/Screenshot 2025-05-03 112329.png>)
+
+## Middleware & Gate Admin  Active
+![alt text](<screenshot/tugas7/Screenshot 2025-05-03 112630.png>)
